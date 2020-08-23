c6b216b4bd6d43983722fb54b8a1aa85ae060853
Abhishek Desai <55031453+abhidesai6@users.noreply.github.com>
2020-08-23 23:06:06 +0530

    week 2

---
+++ Course Assignments/machine-learning-ex2/ex2/costFunctionReg.m
@@ -0,0 +1,18 @@
+function [J, grad] = costFunctionReg(theta, X, y, lambda)
+m = length(y); % number of training examples
+
+
+J = 0;
+grad = zeros(size(theta));
+h = sigmoid(X*theta) ;
+
+J = ((1./m)*( ((-y')*log(h)) - ((1 - y)' * log(1 - h)))) + (lambda/(2*m)) * (theta(2:length(theta)))' * theta(2:length(theta)); 
+
+thetaZero = theta;
+thetaZero(1) = 0;
+
+grad = ((1 / m) * (h - y)' * X) + lambda / m * thetaZero';
+
+
+
+end
